2baf1dbe38ddf11dfb68092d5394e3e36785d7b8
Colby Workman <cgw2319@gmail.com>
2022-02-09 11:32:33 -0700

    post route added

--- main.py
+++ main.py
@@ -1,20 +1,50 @@
 """
 FastAPI - A python framework | Full Course
-https://www.youtube.com/watch?v=7t2alSnE2-I
+https://www.youtube.com/watch?v=7t2alSnE2-I  52:28
 
 
 """
 
+from cgitb import text
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Beevis'}}
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
 
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # specify a limit and published as query parameters
+    # default limit amount is 10 and default published is true, sort is optional
+    if published:
+        return {'data': f'{limit} published blogs from db'}
+    else:
+        return {'data': f'{limit} unpublished blogs from db'}
+
+
+@app.get('/blog/unpublished')
+def get_unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+
+    return {'data': {'1', '2'}}
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': f'blog created with title {request.title}'}
