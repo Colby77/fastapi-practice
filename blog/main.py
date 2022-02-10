74b01e4772dacc5f83dbf57d60996827da584439
Colby Workman <cgw2319@gmail.com>
2022-02-10 14:40:07 -0700

    can create users

--- blog/main.py
+++ blog/main.py
@@ -78,3 +78,15 @@ def delete_blog(id, db: Session = Depends(get_db)):
     db.commit()
 
     return f'Blog with id {id} deleted successfully'
+
+
+@app.post('/user')
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    
+    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
