c4fe0a9442f97bf56275e845ff37725d3b69c1e9
Colby Workman <cgw2319@gmail.com>
2022-02-10 12:02:15 -0700

    delete and update methods added

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Depends
+
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
 
 from . import schemas, models
@@ -17,7 +18,8 @@ def get_db():
         db.close()
 
 
-@app.post('/blog')
+# @app.post('/blog', status_code=201) also works
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
 
     new_blog = models.Blog(title=request.title, body=request.body)
@@ -25,3 +27,53 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get('/blog')
+def get_all_blogs(db: Session = Depends(get_db)):
+
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}', status_code=200)
+def get_blog_by_id(id, response: Response, db: Session = Depends(get_db)):
+
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f'Blog with id {id} not found')
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': f'Blog with id {id} not found'}
+    return blog
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    
+    if blog:
+        blog.update({'title': request.title, 'body': request.body})
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with {id} not found')
+
+    db.commit()
+    return 'updated successfully'
+
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id, db: Session = Depends(get_db)):
+
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+    if blog:
+        blog.delete(synchronize_session=False)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with {id} not found')
+
+    db.commit()
+
+    return f'Blog with id {id} deleted successfully'
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 """
 FastAPI - A python framework | Full Course
-https://www.youtube.com/watch?v=7t2alSnE2-I  1:33:00
-
 
 """
 
