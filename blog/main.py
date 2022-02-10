b45ace3521b35d0cb2fd869760cb63d4a5579970
Colby Workman <cgw2319@gmail.com>
2022-02-10 12:14:00 -0700

    response model created

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,7 @@
 
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
+from typing import List
 
 from . import schemas, models
 from .database import engine, SessionLocal
@@ -29,14 +30,14 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def get_all_blogs(db: Session = Depends(get_db)):
 
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def get_blog_by_id(id, response: Response, db: Session = Depends(get_db)):
 
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
@@ -52,7 +53,7 @@ def get_blog_by_id(id, response: Response, db: Session = Depends(get_db)):
 def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
     
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    
+
     if blog:
         blog.update({'title': request.title, 'body': request.body})
     else:
