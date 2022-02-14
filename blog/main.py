3f3300f684e5ccafe59b0a74494ffc066e803321
Colby Workman <cgw2319@gmail.com>
2022-02-14 16:02:55 -0700

    password hashing added

--- blog/main.py
+++ blog/main.py
@@ -3,8 +3,10 @@ from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 
+
 from . import schemas, models
 from .database import engine, SessionLocal
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -82,8 +84,9 @@ def delete_blog(id, db: Session = Depends(get_db)):
 
 @app.post('/user')
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    
-    new_user = models.User(name=request.name, email=request.email, password=request.password)
+
+    new_user = models.User(name=request.name, email=request.email, 
+                            password=Hash.bcrypt(request.password))
     
     db.add(new_user)
     db.commit()
