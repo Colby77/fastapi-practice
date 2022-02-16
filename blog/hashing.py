fccaf2feca5438b9eab1e34606452385bbf92510
Colby Workman <cgw2319@gmail.com>
2022-02-16 14:53:24 -0700

    login route added

--- blog/hashing.py
+++ blog/hashing.py
@@ -6,3 +6,6 @@ class Hash():
     def bcrypt(password: str):
         hashed_password = password_context.hash(password)
         return hashed_password
+
+    def verify(hashed_password, plain_password):
+        return password_context.verify(plain_password, hashed_password)
